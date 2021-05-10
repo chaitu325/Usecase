9edfd772daa0d7278a919cedabd8c7f8ddafa594
chaitu325 <chaitanya314325@gmail.com>
2021-05-10 15:20:26 +0530

    Results

--- src/read_files.py
+++
@@ -1,5 +0,0 @@
-def from_files(spark, data_dir, file_name, file_format):
-    df = spark.read. \
-        format(file_format). \
-        load(f'{data_dir}/{file_name}.{file_format}', header="True", inferSchema="True")
-    return df
--- src/to_files.py
+++
@@ -1,6 +0,0 @@
-# To write files to parquet format.
-def to_files_parquet(df, tg_dir, tg_file_pattern):
-    df.write. \
-        mode('append'). \
-        parquet(f'{tg_dir}/{tg_file_pattern}')
-
